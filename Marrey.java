0a231f64b13ce5f8f5a13d34c1897b72589e0439
Thierryldb <138627338+Thierryldb@users.noreply.github.com>
2024-10-04 21:03:10 -0300

    Add files via upload

---
+++ Marrey.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Marrey {
+
+    public static void main(String[] args) {
+        int[] num = new int[10];
+        int soma = 0; 
+        double media;
+
+        Scanner scan = new Scanner(System.in);
+
+        for (int i = 0; i < num.length; i++) {
+            System.out.print("Digite o número " + (i + 1) + ": ");
+            num[i] = scan.nextInt();
+            soma += num[i];
+        }
+
+        media = soma / num.length;
+
+        System.out.println("A média é: " + media);
+
+        scan.close();
+    }
+}
